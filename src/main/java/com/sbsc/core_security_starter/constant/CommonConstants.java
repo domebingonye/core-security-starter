8cc752895791d99071e9b88dcbbf15e134ae81a0
domeb <domebingonye@gmail.com>
2026-02-12 06:44:25 +0100

    refactor

--- src/main/java/com/sbsc/core_security_starter/constant/CommonConstants.java
+++ src/main/java/com/sbsc/core_security_starter/constant/CommonConstants.java
@@ -2,6 +2,6 @@ package com.sbsc.core_security_starter.constant;
 
 public class CommonConstants {
     public static final String AUTHORIZATION_KEY = "Authorization";
-    public static final String CLIENT_SECRET="9d0C51%VcI*b5xTs6I*g";
+    public static final String CLIENT_SECRET="9d0C51%VbI*b5xVs6I*h";
     public static final String USERNAME_KEY = "username";
 }
